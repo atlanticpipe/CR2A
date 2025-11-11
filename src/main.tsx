61db4156bc1313b585ba0b4e361a1c0ae9d11009
atlanticpipe <hcarroll@atlanticpipe.us>
2025-11-11 17:32:50 -0500

    Patch v1.06

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./globals.css";
+import "../src/globals.css";
 import App from "../app/App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
