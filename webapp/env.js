dec89d60f6f2cf7badf60c35477ccba50b27f8e8
hewcarroll <hewcarroll@gmail.com>
2026-01-22 13:46:23 -0500

    Delete webapp directory

--- webapp/env.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  "use strict";
-  // GitHub Pages configuration - no AWS services
-  // OpenAI API key will be managed through localStorage
-  window._env = {
-    APP_VERSION: "2.0.0-github-pages",
-    ENVIRONMENT: "github-pages"
-  };
-})();
